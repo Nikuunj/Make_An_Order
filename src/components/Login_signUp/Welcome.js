c5360af0a0602d1b30de38e1c712bae34af4652f
Nikunj <mnikunj262@gmail.com>
2024-05-04 11:48:52 +0530

    welcome of the login & signup

---
+++ src/components/Login_signUp/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+    <div className='text-left'>
+        <p className='font-medium text-5xl text-left'>Welcome !!</p>
+    </div>
+  )
+}
+
+export default Welcome
